b54d45407e2d647d0c56d9212b76af447086ed64
Robert Garza <robstudent1988@gmail.com>
2018-09-28 16:27:51 -0700

    Display boxes and inputs

--- calculator.js
+++ calculator.js
@@ -3,7 +3,22 @@
 var operation = '';
 var num1 = "";
 var num2 = "";
+var bottomString = "";
 
+function updateTopDisplay(stringInput){
+	document.getElementById("topDisplay").value = stringInput;
+}
+
+function clearTopBottom(){
+	bottomString = "";
+	updateTopDisplay("0");
+	updateTopDisplay("0");
+}
+
+function updateBottomDisplay(stringInput) {
+	bottomString += stringInput;
+	document.getElementById("bottomDisplay").value = bottomString;
+}
 
 function buttonclick(input)
 {
